d0c0ff475db62bed0663893f7b647b533a376155
Malick DIOP <diop-malick@hotmail.fr>
2017-05-14 23:40:55 +0000

    minors update

--- .apt_generated/fr/sne/marketplace/service/mapper/UserMapperImpl.java
+++ .apt_generated/fr/sne/marketplace/service/mapper/UserMapperImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 
     value = "org.mapstruct.ap.MappingProcessor",
 
-    date = "2017-05-12T10:24:51+0000",
+    date = "2017-05-13T23:16:48+0000",
 
     comments = "version: 1.1.0.Final, compiler: Eclipse JDT (IDE) 3.12.3.v20170228-1205, environment: Java 1.8.0_121 (Oracle Corporation)"
 
